09cc099226b32079b289fa95c10164f65c9719ee
advya <50411324+Advya@users.noreply.github.com>
2021-06-21 10:12:02 +0300

    failed to display in root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'weclome/index'
   resources :blogposts
   resources :images
   resources :links
+
+  root "weclome#index"
+  
+  get '/' => 'images#index'
+  get '/' => 'links#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
