1b239a0b288458c070a63a38e1db29f5aab3b10c
advya <50411324+Advya@users.noreply.github.com>
2021-06-21 10:21:26 +0300

    figuring out

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :links
 
   root "weclome#index"
-  
-  get '/' => 'images#index'
+  get 'blogposts#index' => 'weclome#index'
+  get '/weclome#index' => 'images#index'
   get '/' => 'links#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
